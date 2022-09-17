bcfce6675f78eda4463de4b49a6cc2332dfca7d9
TanakaMasunda <99711111+TanakaMasunda@users.noreply.github.com>
2022-09-18 01:40:46 +0200

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,11 +1,8 @@
 --  prepares MySQL server for project
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-
 CREATE USER IF NOT EXISTS hbnb_test@localhost;
-    IDENTIFIED BY 'hbnb_test_pwd';
-
+SET PASSWORD FOR hbnb_test@localhost = 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO hbnb_test@localhost;
-
 GRANT SELECT ON perforance_schema.* TO hbnb_test@localhost;
 FLUSH PRIVILEGES;
